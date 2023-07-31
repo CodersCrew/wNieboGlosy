fd7fd051c5503efe981e9f852e4dbe2f744c6336
Marcin <marcinkukulka@gmail.com>
2023-07-31 20:36:38 +0200

    feat: added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
-  ],
-  theme: {
-    fontFamily: {
-      cinzel: ['var(--font-cinzel)'],
-      raleway: ['var(--font-raleway)']
-    }
-  }
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		fontFamily: {
+			cinzel: ['var(--font-cinzel)'],
+			raleway: ['var(--font-raleway)'],
+		},
+		colors: {
+			primary: 'hsl(var(--color-primary))',
+			secondary: 'hsl(var(--color-secondary))',
+			tertiary: 'hsl(var(--color-tertiary))',
+			error: 'hsl(var(--error))',
+			neutral: 'hsl(var(--neutral))',
+			neutralVariant: 'hsl(var(--neutral-variant))',
+			success: 'hsl(var(--success))',
+		},
+	},
 };
