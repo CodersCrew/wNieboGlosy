60fdecbd39d345af7268ef5116533cbb68615039
Marcin <marcinkukulka@gmail.com>
2023-08-03 01:10:26 +0200

    feat: created button with variants

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import { Button } from '@/components/ui/button';
-
 const About = () => {
-  return (
-    <div className="flex gap-5">
-      <Button className="" variant="text">
-        Button Label
-      </Button>
-      <Button className="" variant="text">
-        Button Label
-      </Button>
-    </div>
-  );
+  return <div>page</div>;
 };
 
 export default About;
