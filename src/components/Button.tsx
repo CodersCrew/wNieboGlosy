050a35d6cbf637989e8e757a28f06ffab6be38a4
Marcin Kukułka <marcinkukulka@gmail.com>
2023-08-03 14:26:34 +0200

    Created button (#14)
    * feat: created button with variants
    
    * feat: created button with variants
    
    * Refactor button component
    
    ---------
    
    Co-authored-by: jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>

---
+++ src/components/Button.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import type { ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+import arrow from '@/public/assets/icons/arrow.svg';
+
+const variants = {
+  contained: 'bg-primary font-medium text-white',
+  outlined: 'border border-border bg-transparent font-medium',
+  text: 'gap-1 px-0 font-semibold text-primary decoration-primary underline-offset-4 hover:underline'
+};
+
+const sizes = {
+  desktop: 'gap-4 px-8 py-3 text-xl',
+  mobile: 'gap-3 px-7 py-2.5 text-base'
+};
+
+type ButtonProps = {
+  children?: string;
+  leftArrow?: boolean;
+  rightArrow?: boolean;
+  size?: keyof typeof sizes;
+  variant?: keyof typeof variants;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({
+  children,
+  className,
+  leftArrow,
+  rightArrow,
+  size = 'desktop',
+  variant = 'text',
+  ...props
+}: ButtonProps) => (
+  <button
+    className={twMerge(
+      'flex items-center justify-center rounded-full font-raleway',
+      variants[variant],
+      sizes[size],
+      className
+    )}
+    {...props}
+  >
+    {leftArrow && (
+      <Image
+        alt="strzałka w lewo"
+        className="rotate-180"
+        src={arrow as string}
+      />
+    )}
+    {children}
+    {rightArrow && <Image alt="strzałka w prawo" src={arrow as string} />}
+  </button>
+);
+
+export default Button;
