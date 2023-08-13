9a06b2f21557fa819e6abc2ec85957e1b4cc5325
Jędrzej Ratajczak <nilphym@gmail.com>
2023-08-13 23:28:02 +0200

    Adjust headers and footers (#42)

--- src/app/dolacz/organizacja/page.tsx
+++ src/app/dolacz/organizacja/page.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/Footer';
 
 const JoinOrganization = () => (
   <div className="bg-gradient min-h-screen">
-    <div className="mx-auto max-w-3xl px-5 pt-18 lg:max-w-[1440px] lg:px-30">
+    <div className="mx-auto max-w-3xl px-5 pt-18 lg:max-w-[1440px] lg:px-30 lg:pt-[101px]">
       <div className="flex flex-col items-center gap-8 py-10 lg:flex-row">
         <div className="flex flex-col items-center lg:items-start">
           <h1 className="mb-4 text-center font-cinzel text-2.5xl md:text-6.5xl lg:text-left">
