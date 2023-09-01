5b904722b1cf6be176b49e66687dd4a19e61b8c3
jedrzejratajczak <nilphym@gmail.com>
2023-09-01 18:49:02 +0200

    Update info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Counter from '@/components/Counter';
 
 const WelcomePage = () => {
   return (
-    <div className="grid min-h-screen w-full place-content-center place-items-center gap-y-8 bg-welcomePage bg-cover bg-center font-cinzel text-xl  font-bold text-white md:text-2xl lg:text-3xl ">
+    <div className="grid min-h-screen w-full place-content-center place-items-center gap-y-6 bg-welcomePage bg-cover bg-center font-cinzel text-xl  font-bold text-white md:text-2xl lg:text-3xl ">
       <Link href="/o-nas">
         <p className="rounded-md bg-black/70 px-1 py-2 text-3xl lg:px-5 lg:text-6xl">
           wNieboGłosy
@@ -14,13 +14,13 @@ const WelcomePage = () => {
       <p className="rounded-md bg-black/70 px-1 py-2 lg:px-5">Piąta edycja</p>
       <Counter targetDate="2023-09-01:19:30:00" />
       <p className="max-w-xl rounded-md bg-black/70 px-1 py-2 text-center lg:px-5">
-        Uroczystość Najświętszego Ciała i Krwi Chrystusa Boże Ciało
+        Uroczystość Najświętszego Ciała i Krwi Chrystusa, Boże Ciało
       </p>
       <p className="rounded-md bg-black/70 px-1 py-2 lg:px-5">
-        1 września 2023, godz. 19:30
+        30 maja 2024 r., godz. 19:30
       </p>
       <p className="rounded-md bg-black/70 px-1 py-2 lg:px-5">
-        Wrocław, pl. Wolności
+        Wrocław, pl. Grunwaldzki
       </p>
       <Button
         className="transition-colors hover:bg-bg-primary"
