ab7297f24c7e8c97f52e57a8f1587b5677b7ec31
jedrzejratajczak <nilphym@gmail.com>
2023-09-23 19:40:05 +0200

    test image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,15 @@
-import Link from 'next/link';
-
-import Button from '@/components/Button';
-import Counter from '@/components/Counter';
+import Image from 'next/image';
 
 const WelcomePage = () => {
   return (
     <div className="grid min-h-screen w-full place-content-center place-items-center gap-y-6 bg-welcomePage bg-cover bg-center font-cinzel text-xl  font-bold text-white md:text-2xl lg:text-3xl ">
-      <Link href="/o-nas">
-        <p className="rounded-md bg-black/70 px-1 py-2 text-3xl lg:px-5 lg:text-6xl">
-          wNieboGłosy
-        </p>
-      </Link>
-      <p className="rounded-md bg-black/70 px-1 py-2 lg:px-5">Piąta edycja</p>
-      <Counter targetDate="2024-05-30:19:30:00" />
-      <p className="max-w-xl rounded-md bg-black/70 px-1 py-2 text-center lg:px-5">
-        Uroczystość Najświętszego Ciała i Krwi Chrystusa, Boże Ciało
-      </p>
-      <p className="rounded-md bg-black/70 px-1 py-2 lg:px-5">
-        30 maja 2024 r., godz. 19:30
-      </p>
-      <p className="rounded-md bg-black/70 px-1 py-2 lg:px-5">
-        Wrocław, pl. Grunwaldzki
-      </p>
-      <Button
-        className="transition-colors hover:bg-bg-primary"
-        href="/o-nas"
-        variant="containedWhite"
-      >
-        Przejdź do strony
-      </Button>
+      <Image
+        alt={`Zdjęcie z galerii nr 1`}
+        className="rounded-lg"
+        layout="fill"
+        objectFit="cover"
+        src="https://i.ibb.co/1Z3R7gZ/2022-06-16-191858-5-N7-A5998-2400.jpg"
+      />
     </div>
   );
 };
