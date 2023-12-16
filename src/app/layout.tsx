fa9a17621e4dc611376ac7d3d2ff2da116fdd5f7
jedrzejratajczak <nilphym@gmail.com>
2023-12-16 23:43:54 +0100

    Remove speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 
@@ -16,7 +15,6 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="pl-PL">
       <body className={fonts}>{children}</body>
-      <SpeedInsights />
     </html>
   );
 };
