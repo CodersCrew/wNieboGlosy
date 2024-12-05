466ac03078656fb1f9ba986d26ffcd76e23e4b88
Roksana Rachel <106516178+roksana-rachel@users.noreply.github.com>
2024-12-05 18:38:24 +0100

    67 image skeleton loading (#70)
    Co-authored-by: roksana-rachel <roksana@rachel.gmail.com>

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { useState } from 'react';
 
 const ImageCard = ({
   asset,
@@ -10,18 +11,42 @@ const ImageCard = ({
   index: number;
   setCurrentImage: (index: number) => void;
   setShowModal: (show: boolean) => void;
-}) => (
-  <Image
-    alt={`Zdjęcie z galerii nr ${index}`}
-    className="rounded-lg"
-    layout="fill"
-    objectFit="cover"
-    onClick={() => {
-      setCurrentImage(index);
-      setShowModal(true);
-    }}
-    src={asset}
-  />
-);
+}) => {
+  const [isLoading, setIsLoading] = useState(true);
+  return (
+    <>
+      {isLoading && (
+        <div className="flex h-full w-full animate-pulse items-center justify-center rounded bg-[gray]/10">
+          <svg
+            aria-hidden="true"
+            className="h-10 w-10 text-[gray]"
+            fill="currentColor"
+            viewBox="0 0 20 18"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+          </svg>
+        </div>
+      )}
+      <Image
+        alt={`Zdjęcie z galerii nr ${index}`}
+        className="rounded-lg"
+        layout="fill"
+        objectFit="cover"
+        onClick={() => {
+          setCurrentImage(index);
+          setShowModal(true);
+        }}
+        onError={() => {
+          setIsLoading(false);
+        }}
+        onLoad={() => {
+          setIsLoading(false);
+        }}
+        src={asset}
+      />
+    </>
+  );
+};
 
 export default ImageCard;
